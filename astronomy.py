c27cbc3623773b936c2e1e1ed59baf9932546b72
yudai-22 <progaress21@gmail.com>
2024-11-30 15:00:07 +0900

    Update astronomy.py

--- astronomy.py
+++ astronomy.py
@@ -9,3 +9,9 @@ def ch2v(ch, w):  # チャンネル番号を視線速度 (v: km/s) に変換
     x_tempo, y_tempo, v_tempo = w.wcs_pix2world(0, 0, ch, 0)
     v_kms = v_tempo / 1000.0  # m/s を km/s に変換
     return v_kms
+
+
+def icrs_to_galactic(ra, dec):#赤経赤緯を銀経銀緯へ
+    coord_icrs = SkyCoord(ra=ra * u.deg, dec=dec * u.deg, frame='icrs')  # ICRS座標系(J2000)
+    galactic = coord_icrs.galactic
+    return galactic.l.deg, galactic.b.deg
