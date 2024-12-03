71de7461d654bc8a67843b0e5f0149192e565b64
yudai-22 <progaress21@gmail.com>
2024-12-03 13:42:47 +0900

    Update astronomy.py

--- astronomy.py
+++ astronomy.py
@@ -15,3 +15,24 @@ def icrs_to_galactic(ra, dec):#赤経赤緯を銀経銀緯へ
     coord_icrs = SkyCoord(ra=ra * u.deg, dec=dec * u.deg, frame='icrs')  # ICRS座標系(J2000)
     galactic = coord_icrs.galactic
     return galactic.l.deg, galactic.b.deg
+
+
+def del_header_key(header, keys): # headerのkeyを消す
+    import copy
+    h = copy.deepcopy(header)
+    for k in keys:
+        try:
+            del h[k]
+        except:
+            pass
+    return h
+
+
+def make_new_hdu_integ(hdu, v_start_ch, v_end_ch, w): # 指定速度積分強度のhduを作る
+    data = hdu.data
+    header = hdu.header
+    new_data = np.nansum(data[v_start_ch:v_end_ch+1], axis=0)*np.abs(header["CDELT3"])/1000.0
+    header = del_header_key(header, ["CRVAL3", "CRPIX3", "CRVAL3", "CDELT3", "CUNIT3", "CTYPE3", "CROTA3", "NAXIS3", "PC1_3", "PC2_3", "PC3_3", "PC3_1", "PC3_2"])
+    header["NAXIS"] = 2
+    new_hdu = fits.PrimaryHDU(new_data, header)
+    return new_hdu
